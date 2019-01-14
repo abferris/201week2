b5c24756a384d9d0b31e4506cd431cc3d75d00d4
abferris <abferris@gmail.com>
2019-01-14 14:05:53 -0800

    Object literals created and salmon picture imported.

---
+++ app.js
@@ -0,0 +1,35 @@
+var hours = ['8am', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM']
+var pike={
+  name: '1st and Pike',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+}
+
+var alki ={
+  name: 'Alki',
+  min: 2,
+  max: 16,
+  avg: 4.6,
+}
+
+var capitol ={
+  name: 'Alki',
+  min: 20,
+  max: 38,
+  avg: 2.3,
+}
+
+var seatac ={
+  name: 'Alki',
+  min: 2,
+  max: 16,
+  avg: 1.2,
+}
+
+var center ={
+  name: 'Alki',
+  min: 11,
+  max: 38,
+  avg: 2.3,
+}

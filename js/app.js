c89ccd7b96d221b721f2705f1d0cab550012a348
abferris <abferris@gmail.com>
2019-01-19 06:49:02 -0800

    touching up layout so everything is the same and pictures don't go to the edge

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var hours = ['6 AM', '7 AM','8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
 
 //creating hours with total on the end
-var hoursPlus=[];
+       var hoursPlus=[];
 for (i=0; i<hours.length; i++){
   hoursPlus[i]= hours[i];
 }
